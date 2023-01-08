d70380b8d53806d7d0c2e8916d072676c1be26c2
Eyal-donut <LilaEyal@gmail.com>
2023-01-08 12:59:46 +0100

    commit_1.1

---
+++ 01_Primitive_Data_Types/task_1.1_primitive_data_types.js
@@ -0,0 +1,18 @@
+let string = 'dolphin';
+let num = 1;
+let amIHungry = true;
+let name = null;
+let b = undefined;
+const person = {};
+const sym = Symbol();
+
+console.log(typeof string);
+console.log(typeof num);
+console.log(typeof amIHungry);
+console.log(typeof name);
+console.log(typeof b);
+console.log(typeof person);
+console.log(typeof sym);
+
+
+
