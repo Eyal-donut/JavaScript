bb23ab9e5c7c5d16b8cb0ec5426b783b57707844
Eyal-donut <LilaEyal@gmail.com>
2023-01-08 16:26:18 +0100

    2_1_functions

---
+++ 02_functions/assginments_pdfs/2.1_functions.js
@@ -0,0 +1,18 @@
+function countryInfo(country, population, capitalCity) {
+    console.log(`${country} has ${population} million people and its capital city is ${capitalCity}.`);
+    let countryOne = countryInfo('Spain', 47, 'Madrid');
+    let countryTwo =countryInfo('Israel',9,'Jerusalem');
+    let countryThree =countryInfo('Austria',9,'Vienna');
+    return countryOne, countryTwo, countryThree 
+}
+
+
+
+
+
+    
+    
+    
+   
+
+ 
