bd937b11526d8e8f2dc555d4a9b9ada54b44dc51
Eyal-donut <LilaEyal@gmail.com>
2023-01-09 12:39:17 +0100

    3.3_task

--- 03_Different_Ways to_Declare_Functions/tempCodeRunnerFile.js
+++ 03_Different_Ways to_Declare_Functions/tempCodeRunnerFile.js
@@ -1,15 +1,1 @@
-function power(a) {
-// let myNumber = a;
-// let result = Math.pow(myNumber, 2);
-// return result;
-// }
-// function add(a, b = 5) {
-// let myNumber = a;
-// let sum = myNumber + b;
-// return sum;
-// }
-// // From function expressions to function declarations
-// const hello = () => "Hello!";
-// const squareRoot = a => Math.sqrt(a);
-// const randomNumbers = (a, b) => Math.random() * (a - b) +
-// b;
+const welcome = () => {return 'Welcome to Appleseeds Bootcamp!'}
