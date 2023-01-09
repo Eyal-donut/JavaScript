2acb8225c2f5d82040f8a93ecbaf823f3f32bcb6
Eyal-donut <LilaEyal@gmail.com>
2023-01-09 13:06:14 +0100

    commit_3.3_02

---
+++ 03_Different_Ways to_Declare_Functions/3.3.js
@@ -0,0 +1,18 @@
+function percentageOfWorld1(population){
+    return (population/7900)*100;
+}
+let percentageChina = percentageOfWorld1(1441);
+let percentageIsrael = percentageOfWorld1(9);
+let percentageRussia = percentageOfWorld1(143);
+console.log(percentageChina)
+console.log(percentageIsrael)
+console.log(percentageRussia)
+
+
+const percentageOfWorld2 = (population) => (population/7900)*100;
+let percentageChina2 = percentageOfWorld2(1441);
+let percentageIsrael2 = percentageOfWorld2(9);
+let percentageRussia2 = percentageOfWorld2(143);
+console.log(percentageChina2)
+console.log(percentageIsrael2)
+console.log(percentageRussia2)
