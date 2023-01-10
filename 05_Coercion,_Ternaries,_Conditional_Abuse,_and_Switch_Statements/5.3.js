93003e1fe3fb9eb271f4d2f88d676d0354b6461a
Eyal-donut <LilaEyal@gmail.com>
2023-01-10 13:47:42 +0100

    commit 5.4

--- 05_Coercion,_Ternaries,_Conditional_Abuse,_and_Switch_Statements/5.3.js
+++ 05_Coercion,_Ternaries,_Conditional_Abuse,_and_Switch_Statements/5.3.js
@@ -1,1 +1,17 @@
-console.log('hi')
+  const passwordValidation1 = (password) => {
+      if (password.length > 7) {
+          return 'Strong';
+      } else {
+          return 'Weak';
+     }
+ }
+ console.log(passwordValidation1('237423uzdjhfwg'))
+
+ const passwordValidation2 = (password) => password.length > 7 ? console.log('Strong') : console.log('Weak');
+ passwordValidation2('2')
+
+const passwordValidation3 = (password) => {
+    password.length > 7 && /[A-Z]/.test(password) ? console.log('Very Strong') : password.length > 7 ? console.log('Strong') : console.log('Weak');
+    }
+passwordValidation3('2AAAAAAA')
+
