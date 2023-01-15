ab54efeffb3a0d13f986c1e254c92059234185ba
Eyal-donut <LilaEyal@gmail.com>
2023-01-15 20:16:32 +0100

    commit_10.1_done

--- 10_callback_functions/10.1.js
+++ 10_callback_functions/10.1.js
@@ -9,3 +9,40 @@ const isString = (str, callBackfunc) => {
     }
 }
 isString(56, callBackfunc)
+
+function createDashesFunc(str) {
+  let arr = [];
+  for (let k = 0; k < str.length; k++) {
+    arr.push(str[k]);
+    arr.push("-");
+  }
+  arr.pop();
+  return arr.toString().replaceAll(",", " ");
+}
+
+
+let firstWord = [];
+const firstWordUpperCase = (sent, createDashesFunc) => {
+  for (let j = 0; j <= sent.length; j++) {
+    if (sent[j] !== " ") {
+      firstWord.push(sent[j].toUpperCase());
+    } else {
+      break;
+    }
+  }
+  console.log(createDashesFunc(firstWord));
+};
+firstWordUpperCase("The boy ate poo", createDashesFunc);
+
+function addLove(string) {
+    string = (string + " <3")
+    console.log(string)
+}
+const spreadLove = (arg, addLove) => {
+    if (typeof arg === 'string') {
+        addLove(arg);
+    } else {
+        console.log('we only love strings')
+    }
+}
+spreadLove('hi', addLove)
