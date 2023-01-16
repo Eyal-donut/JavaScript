9bdefe5e289675bd4aaa2658c172d6ea9ebb0404
Eyal-donut <LilaEyal@gmail.com>
2023-01-16 09:49:38 +0100

    11.1

---
+++ 11.maps_and_forEeach/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+function getMovieData(data) {
+//   const objArray = [];
+//   data.forEach(function (release) {
+//     objArray.push({ id: release.id, title: release.title });
+//   });
+//   return objArray;
+// }
+// console.log(getMovieData(newReleases));
