1bcf935ddba2d1c5ce16cc42bd8cbfb2a0a373e1
Eyal-donut <LilaEyal@gmail.com>
2023-01-25 13:05:14 +0100

    21.2

---
+++ 20_DOM_Eventlisteners/21.2.js
@@ -0,0 +1,17 @@
+const h1 = document.querySelector("h1");
+const buttonPlus = document.querySelector("button.plus");
+const buttonMinus = document.querySelector("button.minus");
+
+const declaration = window.getComputedStyle(h1);
+let fontSizeValue = parseFloat(declaration.getPropertyValue('font-size'));
+
+
+buttonPlus.addEventListener('click', function increaseSize(){
+    fontSizeValue < 100 ? ++fontSizeValue : "";
+    h1.style.fontSize = `${fontSizeValue}px`
+})
+
+buttonMinus.addEventListener('click', function decreaseSize(){
+    fontSizeValue > 6 ? --fontSizeValue : "";
+    h1.style.fontSize = `${fontSizeValue}px`
+})
