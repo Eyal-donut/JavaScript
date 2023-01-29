2b3be3b9cb38eece117dbb9d5832dc4472e85eeb
Eyal-donut <LilaEyal@gmail.com>
2023-01-29 20:32:17 +0100

    22.3_done

--- 22_Prevent_Default_and_Form_Events/22.3.js
+++ 22_Prevent_Default_and_Form_Events/22.3.js
@@ -2,18 +2,50 @@ const wrong = document.querySelector(".wrong");
 const right = document.querySelector(".right");
 const playAgain = document.querySelector(".play-again");
 const button = document.querySelector("button");
+const keyGuessed = document.querySelector(".keys-guessed");
+const letterBox = document.querySelector(".letter-box");
+const regex = /^[a-zA-Z]*$/
 
-function toggleDisplay(variable) {
-  variable.classList.toggle("display");
+function addClass(variable) {
+  variable.classList.add("display");
 }
 
-
-function randomCharacter() {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-   const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
-   return randomCharacter
+function removeClass(variable) {
+  variable.classList.remove("display");
 }
 
-window.addEventListener('keypress', function(e){
+function makeRandomChar() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const randomChar = alphabet[Math.floor(Math.random() * alphabet.length)];
+  return randomChar;
+}
 
-})
+function handler(e) {
+    const keyPressed = e.key
+    if (keyPressed !== makeRandomChar()) {
+      addClass(wrong);
+      if (regex.test(keyPressed) && !keyGuessed.textContent.includes(`${keyPressed}`)){
+      keyGuessed.textContent += `${keyPressed}, `;
+      }
+    } else {
+      addClass(right);
+      addClass(playAgain);
+      addClass(button);
+      removeClass(wrong);
+      letterBox.textContent = keyPressed;
+      window.removeEventListener("keypress", handler)
+    }
+}
+
+window.addEventListener("keypress", handler)
+
+button.addEventListener("click", function (e) {
+    removeClass(right);
+    removeClass(playAgain);
+    removeClass(button);
+    keyGuessed.textContent = ''
+    letterBox.textContent = '?';
+    makeRandomChar();
+    window.addEventListener("keypress", handler);
+});
+
