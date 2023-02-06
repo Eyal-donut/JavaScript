c56cf0c5656efcded29d649b256d726777c816db
Eyal-donut <LilaEyal@gmail.com>
2023-02-06 17:14:43 +0100

    done

--- 29_Async_and_await/29.1.js
+++ 29_Async_and_await/29.1.js
@@ -1,50 +1,83 @@
-const button = document.querySelector("button");
-const titleElement = document.querySelector("h2");
-const textElement = document.querySelector("p");
+// const getIDs = () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve([532, 543, 753, 1, 5]);
+//     }, 1500);
+//   });
 
-button.addEventListener("click", async function () {
-  try {
-    // const response = await axios.get("https://api.jokes.one/jod");
-    // const data = response.json()
-    const data = {
-        success: {
-          total: 1
-        },
-        contents: {
-          jokes: [
-            {
-              description: "Joke of the day ",
-              language: "en",
-              background: "",
-              category: "jod",
-              date: "2023-02-06",
-              joke: {
-                title: "Knock Knock - Opera who?",
-                lang: "en",
-                length: "111",
-                clean: "1",
-                racial: "0",
-                date: "2023-02-06",
-                id: "oFr2gwJ7UpCrRffHKbXksQeF",
-                text: "Knock Knock\r\nWho's there?\r\nOpera!\r\nOpera who?\r\nOpera-tunity, and you thought opportunity only knocked once!"
-              }
-            }
-          ],
-          copyright: ""
-        }
-      }
-    const title = data.contents.jokes[0].joke.title;
-        titleElement.innerText = title;
-        const joke = data.contents.jokes[0].joke.text;
-        textElement.innerText = joke;
-  } 
-  catch (err) {
-    console.log("Something went wrong");
-    console.log(err);
-  }
-});
+// // async function getIDs() {
+// //   setTimeout(() => {
+// //     const response = await[(532, 543, 753, 1, 5)];
+// //     return response;
+// //   }, 1500);
+// // }
 
+// // async function getRecipe(recipeID) {
+// //   try {
+// //     setTimeout(
+// //       (ID) => {
+// //         const recipe = {
+// //           title: "Fresh tomato pasta",
+// //           publisher: "Pinchas Hodadad",
+// //         };
+// //         const recipeGot = await`${ID}: ${recipe.title}`;
+// //         return recipeGot;
+// //       },
+// //       1500,
+// //       recipeID
+// //     );
+// //   } catch (error) {
+// //     console.log("It is an error");
+// //   }
+// // }
 
+// const getRecipe = (recipeID) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(
+//       (ID) => {
+//         const recipe = {
+//           title: "Fresh tomato pasta",
+//           publisher: "Pinchas Hodadad",
+//         };
+//         resolve(`${ID}: ${recipe.title}`);
+//       },
+//       1500,
+//       recipeID
+//     );
+//   });
+// };
 
+// getIDs()
+//   .then((IDs) => {
+//     console.log(IDs);
+//     return getRecipe(IDs[2]);
+//   })
+//   .then((recipe) => {
+//     console.log(recipe);
+//   })
+//   .catch((error) => {
+//     console.log("It is an error!");
+//   });
 
+//get an array of ids(result is array), get a receipe with the id with the index of two (result: ID: recepit name)
+
+async function getReceiptAndID (){
+    try {
+        let response = []
+    setTimeout(()=> {
+        response = [532, 543, 753, 1, 5]
+    }, 1500)
+        console.log(response)
+        const receipt = {
+            title: "Fresh tomato pasta",
+            publisher: "Pinchas Hodadad",
+          };
+        const ID = await response[2]
+          console.log(`${await ID}: ${receipt.title}`)
+}catch (error){
+    console.log('It is an error')
+}
+}
+
+getReceiptAndID()
 
---
+++ 29_Async_and_await/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+  async function getIDs(){
+//     setTimeout(() => {
+//         const response = await [532, 543, 753, 1, 5]
+//     }, 1500)
+//   }
