acf8227463f50be546a404be0f8828c0cadabf62
Eyal-donut <LilaEyal@gmail.com>
2023-02-07 10:24:37 +0100

    30.1_in_work

--- 29_Async_and_await/29.2.js
+++ 29_Async_and_await/29.2.js
@@ -4,36 +4,36 @@ const textElement = document.querySelector("p");
 
 button.addEventListener("click", async function () {
   try {
-    // const response = await fetch("https://api.jokes.one/jod");
+    const response = await fetch("https://api.jokes.one/jod");
     if (!response.ok) throw "Something went wrong"
-    // const data = await response.json()
-    const data = {
-        success: {
-          total: 1
-        },
-        contents: {
-          jokes: [
-            {
-              description: "Joke of the day ",
-              language: "en",
-              background: "",
-              category: "jod",
-              date: "2023-02-06",
-              joke: {
-                title: "Knock Knock - Opera who?",
-                lang: "en",
-                length: "111",
-                clean: "1",
-                racial: "0",
-                date: "2023-02-06",
-                id: "oFr2gwJ7UpCrRffHKbXksQeF",
-                text: "Knock Knock\r\nWho's there?\r\nOpera!\r\nOpera who?\r\nOpera-tunity, and you thought opportunity only knocked once!"
-              }
-            }
-          ],
-          copyright: ""
-        }
-      }
+    const data = await response.json()
+    // const data = {
+    //     success: {
+    //       total: 1
+    //     },
+    //     contents: {
+    //       jokes: [
+    //         {
+    //           description: "Joke of the day ",
+    //           language: "en",
+    //           background: "",
+    //           category: "jod",
+    //           date: "2023-02-06",
+    //           joke: {
+    //             title: "Knock Knock - Opera who?",
+    //             lang: "en",
+    //             length: "111",
+    //             clean: "1",
+    //             racial: "0",
+    //             date: "2023-02-06",
+    //             id: "oFr2gwJ7UpCrRffHKbXksQeF",
+    //             text: "Knock Knock\r\nWho's there?\r\nOpera!\r\nOpera who?\r\nOpera-tunity, and you thought opportunity only knocked once!"
+    //           }
+    //         }
+    //       ],
+    //       copyright: ""
+    //     }
+    //   }
     const title = data.contents.jokes[0].joke.title;
         titleElement.innerText = title;
         const joke = data.contents.jokes[0].joke.text;
---
+++ 30_APIs/30.1.js
@@ -0,0 +1,2 @@
+//search - when you press search: fetch from repo.
+// function createUser: create all the needed elements, call function insertUserInfo and append them
